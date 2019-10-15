e7ae3787cd05c87ce75998ec8918851058438bb9
EditYJ <158392613@qq.com>
2019-10-15 08:39:02 +0800

    处理响应数据，支持promise链式调用

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosResponse } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
 import { transformRequest } from './helpers/data'
 import { processHeaders } from './helpers/headers'
 
-function Axios(config: AxiosRequestConfig): void {
+function Axios(config: AxiosRequestConfig): Promise<AxiosResponse> {
   processConfig(config)
-  xhr(config)
+  return xhr(config)
 }
 
 function processConfig(config: AxiosRequestConfig): void {
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,20 +1,40 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosResponse } from './types'
 
-export default function xhr(config: AxiosRequestConfig): void {
-  const { url, method = 'get', data = null, headers } = config
+export default function xhr(config: AxiosRequestConfig): Promise<AxiosResponse> {
+  return new Promise<AxiosResponse>((resolve, reject) => {
+    const { url, method = 'get', data = null, headers, responseType } = config
 
-  const request = new XMLHttpRequest()
-
-  request.open(method.toUpperCase(), url, true)
-
-  // 放置头部Headers
-  Object.keys(headers).forEach((name) => {
-    if (data === null && name.toLowerCase() === 'content-type') {
-      delete headers[name]
-    } else {
-      request.setRequestHeader(name, headers[name])
+    const request = new XMLHttpRequest()
+    if (responseType) {
+      request.responseType = responseType
     }
-  })
+    request.open(method.toUpperCase(), url, true)
+    // 配置响应对象
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const responseHeaders = request.getAllResponseHeaders()
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      resolve(response)
+    }
 
-  request.send(data)
+    // 放置头部Headers
+    Object.keys(headers).forEach((name) => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+    request.send(data)
+  })
 }
