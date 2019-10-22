508efd7a77133128b082ee19931f4e928be2a0ad
EditYJ <158392613@qq.com>
2019-10-22 15:40:29 +0800

    ts-axios接口拓展-混合对象实现

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,6 +1,6 @@
 const toString = Object.prototype.toString
 
-export function isDate(val: any): val is Date{
+export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
@@ -10,4 +10,11 @@ export function isDate(val: any): val is Date{
 
 export function isPlainObject(val: any): val is Object {
   return toString.call(val) === '[object Object]'
+}
+
+export function extend<T, U>(to: T, from: U): T & U {
+  for (const key in from) {
+    (to as T & U)[key] = from[key] as any
+  }
+  return to as T & U
 }
