945e2251fb67649b7dacea30e5555d027f608a49
EditYJ <158392613@qq.com>
2019-10-22 16:35:22 +0800

    demo编写

--- examples/server.js
+++ examples/server.js
@@ -119,6 +119,8 @@ router.get('/error/timeout', function(req, res) {
   }, 3000)
 })
 
+registerExtendRouter()
+
 app.use(router)
 
 const port = process.env.PORT || 8012
@@ -126,3 +128,46 @@ const port = process.env.PORT || 8012
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
+
+function registerExtendRouter () {
+  router.get('/extend/get', function(req, res) {
+    res.json({
+      msg: 'hello world'
+    })
+  })
+
+  router.options('/extend/options', function(req, res) {
+    res.end()
+  })
+
+  router.delete('/extend/delete', function(req, res) {
+    res.end()
+  })
+
+  router.head('/extend/head', function(req, res) {
+    res.end()
+  })
+
+  router.post('/extend/post', function(req, res) {
+    res.json(req.body)
+  })
+
+  router.put('/extend/put', function(req, res) {
+    res.json(req.body)
+  })
+
+  router.patch('/extend/patch', function(req, res) {
+    res.json(req.body)
+  })
+
+  router.get('/extend/user', function(req, res) {
+    res.json({
+      code: 0,
+      message: 'ok',
+      result: {
+        name: 'jack',
+        age: 18
+      }
+    })
+  })
+}
