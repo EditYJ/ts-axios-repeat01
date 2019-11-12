5161ce15bfe0dac8c53fda5ae151ab8fb3c1a090
EditYJ <158392613@qq.com>
2019-11-12 15:52:39 +0800

    静态方法扩展

--- src/axios.ts
+++ src/axios.ts
@@ -16,12 +16,25 @@ function createInstance(config: AxiosRequestConfig): AxiosStatic {
 
 const axios = createInstance(defaults)
 
+axios.create = function create(config) {
+  return createInstance(mergeConfig(defaults, config))
+}
+
 axios.CancelToken = CancelToken
 axios.Cancel = Cancel
 axios.isCancel = isCancel
 
-axios.create = function create(config) {
-  return createInstance(mergeConfig(defaults, config))
+axios.all = function all(promises) {
+  return Promise.all(promises)
 }
+
+axios.spread = function spread(callback) {
+  return function wrap(arr) {
+    return callback.apply(null, arr)
+  }
+}
+
+axios.Axios = Axios
+
 
 export default axios
